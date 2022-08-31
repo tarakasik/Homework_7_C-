a8760a4b6167caddb42a166ac1ce1e42874aaa13
Stanislav Volokhov <lamantina123@gmail.com>
2022-08-31 04:29:22 +0400

    add task52

--- Task#50/Program.cs
+++ Task#50/Program.cs
@@ -1,43 +1,41 @@
-﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
-// и возвращает значение этого элемента или же указание, что такого элемента нет.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// 17 -> такого числа в массиве нет
+﻿Random rand = new Random();
+int[,] array = new int[5,5];
+FillMatrix(array);
+Console.WriteLine("My array:");
+Console.Write("Input line: ");
+int findRows = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Input column: ");
+int findColumns = Convert.ToInt32(Console.ReadLine()) - 1;
 
-int[,] myArray = new int[5, 5];
-Random rand = new Random();
-int positionRows = 0;
-int positionColumns = 0;
-
-void FindElement(int[,] myArray)
+void FillMatrix(int[,] array)
 {
-    for (int i = 0; i < myArray.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < myArray.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            myArray[i, j] = rand.Next(1, 9);
-            Console.Write($"{myArray[i, j]}\t");
-            positionColumns++;
+            array[i, j] = rand.Next(1, 15);
+            Console.Write($"{array[i, j]}\t");
         }
-        Console.WriteLine();
-        positionRows++;
-    }
+    }Console.WriteLine();
 }
-Console.WriteLine("Input array rows: ");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input array columns: ");
-int columns = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
-FindElement(myArray);
 
-if (rows <= positionRows && columns <= positionColumns)
+for (int i = 0; i < array.GetLength(0); i++)
 {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        // array[i, j] = rand.Next(1, 15);
+        // Console.Write($"{array[i, j]}\t");
+    }
     Console.WriteLine();
-    Console.WriteLine($"Number : {myArray[rows - 1, columns - 1]}");
+}
+if (findRows < 0 | findRows > array.GetLength(0) - 1 | findColumns < 0 | findColumns > array.GetLength(1) - 1)
+{
+    Console.WriteLine("This element is mising in this array");
 }
 else
 {
-    Console.WriteLine("This element is missing in this array !!!");
+    Console.WriteLine("Element number array = {0}", array[findRows, findColumns]);
 }
+Console.ReadLine();
+
+
---
+++ Task#52/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] array = new int[5, 5];
+Random rand = new Random();
+Console.WriteLine("Input array rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input array columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Array");
+Console.WriteLine();
+
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = rand.Next(1, 10);
+        Console.Write($"{array[i, j]}\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine($"Number columns array '{array.GetLength(1)}'");
+Console.WriteLine();
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.Write($"{sum / array.GetLength(0)}\t");
+}
+Console.Write(" <= Arithmetic average of each column in the array");
